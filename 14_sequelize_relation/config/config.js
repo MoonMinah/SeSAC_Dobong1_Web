fe0ae468786e152e8433eb3abccd1490a3923e6a
MoonMina <mingke48@gmail.com>
2024-02-22 17:14:13 +0900

    sequelize

--- 14_sequelize_relation/config/config.js
+++ 14_sequelize_relation/config/config.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const development = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: "root",
+  database: "sesac",
   host: "127.0.0.1",
   dialect: "mysql",
 };
