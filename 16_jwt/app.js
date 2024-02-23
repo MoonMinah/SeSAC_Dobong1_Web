970ba82fe7929e312a264124855fb241620d0b2b
MoonMina <mingke48@gmail.com>
2024-02-23 15:36:41 +0900

    jwt

--- 16_jwt/app.js
+++ 16_jwt/app.js
@@ -2,14 +2,11 @@ const express = require("express");
 const app = express();
 const PORT = 9090;
 const jwt = require("jsonwebtoken");
-const SECRET = "p0DVLwue0qC0PTbymqW24hLwyUPy7D"; //랜덤 문자열
+const SECRET = "Um54Y4szBu0LOidWxmUDl7S3RodtPp"; //랜덤 문자열
 
-// middleware
 app.set("views", "./views");
 app.set("view engine", "ejs");
-//app.use("/static", express.static(__dirname + "/static"));
 
-// body-parser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -28,26 +25,59 @@ app.get("/login", (req, res) => {
 app.post("/login", (req, res) => {
   try {
     console.log(req.body);
-    res.send("서버 콘솔 확인");
-
-    const { id, pw } = req.body;
+    // res.send("서버 콘솔 확인");
     const { id: realId, pw: realPw } = userInfo;
+    const { id, pw } = req.body;
+
     if (id === realId && pw === realPw) {
-      // jtw 인증 생성 토큰
-      // const token = jwt.sign(payload, secret/private key, option)
+      //jwt 인증 토큰 생성
+      //   const token = jwt.sign(payload, secret/private key, option)
       const token = jwt.sign({ id: id }, SECRET);
       console.log(token);
-      res.send({ result: true, token });
+      res.send({ result: true, token: token });
     } else {
       res.send({ result: false, message: "로그인 정보가 올바르지 않습니다." });
     }
-  } catch (err) {
-    console.log("POST/login", err);
+  } catch (error) {
+    console.log("POST /login", err);
     res.status(500).send("server error");
   }
 });
-app.post("/token", (req, res) => {});
 
+//token 정보 확인
+app.post("/token", (req, res) => {
+  try {
+    if (req.headers.authorization) {
+      console.log(req.headers.authorization);
+      //Bearer 빼고 token 값만 저장하기 위해서
+      const token = req.headers.authorization.split(" ")[1];
+
+      try {
+        console.log("token:::", token); //token::: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImNvY29hIiwiaWF0IjoxNzA4NjU2MDI0fQ.TjLDn_y6iTxRS2Q_T5MYg7Rmykd_By0Z90OGE98StRE
+        const auth = jwt.verify(token, SECRET);
+        console.log("auth:::", auth);
+        // verify()의 리턴값 => auth::: { id: 'cocoa', iat: 1708656024 }
+        // iat : issued at, 발급된 시간, 토큰이 만든지 얼만큼 됐는지 알수있음
+
+        if (userInfo.id === auth.id) {
+          res.send({ result: true, name: userInfo.name });
+        }
+        res.end();
+      } catch (err) {
+        // 잘못된 정보가 들어왔을 때
+        console.log("토큰 인증 에러", err);
+        res.send({ result: false, message: "인증된 회원이 아닙니다." });
+      }
+    } else {
+      // 인증 정보 안들어왔을때
+      res.redirect("/login");
+    }
+  } catch (err) {
+    console.log("POST /token", err);
+    res.status(500).send("server error");
+  }
+});
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
+  console.log(`http://http://101.101.209.101:${PORT}`);
 });
